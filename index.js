622778ae115c2d845d9fd2a5a0287e65f3574395
Madeline Celis <celiscb@vcu.edu>
2018-09-17 17:15:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function produceDrivingRange(num){
   return function(begStr, endStr){
-    let begStr1 = begStr.slice(0, 2);
-    console.log(begStr1);
-    let begNum = parseInt(begStr1);
-    let endStr1 = endStr.slice(0, 2);
-    let endNum = parseInt(endStr1);
+    let begNum = parseInt(begStr.slice(0, 2));
+    let endNum = parseInt(endStr.slice(0, 2));
 
     let blocks = Math.abs(endNum - begNum);
     if(blocks > num){
@@ -28,5 +25,4 @@ function createDriver(){
     this.name = name;
     this.id = ++id;
   };
-
 };
